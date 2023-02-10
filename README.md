c0bcdb1f2c95e918d93048bfdb9ab51b56c68e6b
LINHUANG0 <120541669+LINHUANG0@users.noreply.github.com>
2023-02-10 13:28:17 +1300

    fix bug and add report in readme
    fix bug and add report in readme

--- README.md
+++ README.md
@@ -1,13 +1,89 @@
-# 636S12023webapp
+# 636S12023webapp Project Report
 
-COMP636 Web App
+ 
+
+## 1.Structure of solution
+### Routes and Functions
+
+#### 1.1 Basic
+This structure outlines a Flask application with three routes: "/", "/route", and "/staff".
+
+The "/" route is the default or home route of the application, which returns a template called "route.html" when accessed. This template would contain the HTML for the home page of the application.
+
+The "/route" route is a public route that can be accessed by anyone. It also returns the "route.html" template.
+
+The "/staff" route is a staff-only route that requires some form of authentication or authorization to access. This route returns a template called "staffroute.html". This template would contain the HTML for a page that is only accessible to staff members.
+
+In each of these routes, the render_template function is used to return a specific HTML template. The template name is passed as an argument to this function, and the function returns the template along with any data that needs to be displayed in the template. This allows the routes to display dynamic content based on the data passed to them.
+
+#### 1.2 Books
+The solution has 6 routes defined regarding books, each route corresponds to a unique web page or endpoint. The routes are /listbooks, /searchbooks, /loanbook, /currentloans, /loan/add, and /returnbook. The following is a brief overview of the main functions associated with each route:
+
+The "/listbooks" route  fetches data from the database using an SQL query and passes it to the booklist.html template for rendering. The query retrieves all loan information for books that have been returned.
+
+The "/searchbooks" route is a POST route that takes input from a search form and retrieves data from the database based on the search criteria. The user can search for books by title or author. The query retrieves all loan information for books that have been returned and matches the search criteria.
+
+The "/loanbook" route is a GET route that retrieves a list of all borrowers and all books that are available for loan. If a borrower ID is provided as a query parameter, the list of borrowers will be filtered to only show the borrower with the matching ID. The data retrieved from the database is passed to the addloan.html template for rendering.
+
+The "/currentloans" route fetches data from the database using an SQL query and passes it to the currentloans.html template for rendering. The query retrieves all loan information for books that have not been returned.
+
+The "/loan/add" route is responsible for adding a new loan to the database and is accessed using the HTTP POST method. The endpoint takes in the borrower ID, book ID, and loan date from the request form, creates a connection to the database using the getCursor() function, and inserts a new loan record into the "loans" table with the returned value set to 0. After inserting the new loan, the endpoint redirects the user to the "/currentloans" route.
+
+The "/returnbook" route is responsible for marking a loan as returned and is accessed using the HTTP GET method. The endpoint takes the loan ID as a parameter from the request URL and updates the corresponding loan record in the "loans" table with the returned value set to 1. After updating the loan, the endpoint redirects the user to the "/currentloans" route.
+
+#### 1.3 Borrowers
+The listborrowers function retrieves all the borrowers from the database and displays them in a table in the borrowerlist.html template. 
+
+The searchborrower function allows the user to search for a borrower by either the borrower ID or the borrower's name. If the search term is a number, it searches for the borrower by the borrower ID, otherwise it searches by the borrower's name. 
+
+The borrowerdetail function displays detailed information about a borrower when the borrower's ID is provided as a query parameter. 
+
+The addborrower function provides a form for adding a new borrower to the database. The form data is submitted to the server and the new borrower is saved to the database. 
+
+Finally, the updateborrower function allows the user to update the information of an existing borrower in the database.
+
+#### 1.4 Reports
+At last, The solution consists of 3 reports which are accessible via 3 different routes. The first route "/overduebookslist" returns a report of books that are currently overdue. The second route "/listloansumary" returns a report that summarizes the total number of loans for each book. The third route "/listborrowersummay" returns a report that summarizes the total number of loans for each borrower.
+
+All the routes make use of the function getCursor() to create a database connection. The function then executes an SQL query and fetches the results of the query. Finally, the results are passed to a template file that generates the report in HTML format. The template files for the 3 reports are "overduebookslist.html", "listloansumary.html", and "listborrowersummay.html" respectively. The data passed from the functions to the templates are overduelist, loanlist, and borrowerlist.
+
+## 2. Assumptions and design decisions
+### 2.1 Share a page template
+Use two separate pages between public and staff, because the functions inside the pages are different. Employees can use more functions, but the public is relatively simple to search books. In the search results, the same function is called and the same results page is used, because there is no difference in the search for books.
+
+### 2.1 GET and POST methods
+By limiting the HTTP methods that each endpoint can handle, the code helps to ensure that the application behaves as expected and that sensitive data is not being exposed. For example, a GET request is typically used to retrieve data, whereas a POST request is used to submit data to a server. 
+
+In the project, the GET method is used for searching data, and the POST method is used for issue or return books or add new borrowers.
 
 
+## 3.Discussion: What changes would be required if my application was to support multiple library branches
+
+### Changes required to database tables and the design and implementation 
+
+#### Add tables
+First, we must create a new table 'library'. The libarary table should contain: libararyid INT, libararyname VARCHAR(45)
+
+we could also add 'account' table: accountid INT, admin TINYINT, password VARCHAR(45), libararyid INT 
+
+We need to display different library names according to the library, add account functions on the right side of the page, and add functions such as login and logout if possible.
+
+#### Foreign keys
+Parent table and field(s) --  Child table and field(s)
+
+library.libararyid    -- account.libararyid
+
+
+#### Modify tables
+If all libraries share books, you don't need to associate books with boocopies, but you need to add libararyid to the borrowers table, because borrowers belong to different libraries:
+
+Foreign keys
+Parent table and field(s)   --  Child table and field(s)
+library.libararyid   --  borrowers.libararyid
+
+If we want to increase the location of the book, we could add libararyid in the bookcopies table. When borrowing a book, update this field to the libararyid of the library where the borrower is located. When returning the book, you need to update this field to the returned libararyid.
+
+
+### other
 TODO confirm search publicsearch result
-TODO searchresultbooklist.html: change to due day
 
-Add 
-1.search books by staff
-2.search borrower by staff
-3.issue books 
-4.return books
--- librarywebapp/app.py
+++ librarywebapp/app.py
@@ -31,6 +31,17 @@ def getCursor():
 def home():
     return render_template("route.html")
 
+# Add public access /route
+@app.route("/route")
+def publicroute():
+    return render_template("route.html")
+
+# Add staff access /route
+@app.route("/staff")
+def staffroute():
+    return render_template("staffroute.html")
+
+#  Books function 
 @app.route("/listbooks")
 def listbooks():
     connection = getCursor()
@@ -47,76 +58,17 @@ def listbooks():
     bookList = connection.fetchall()
     return render_template("booklist.html", booklist = bookList)    
 
-@app.route("/loanbook", methods=["GET"])
-def loanbook():
-    todaydate = datetime.now().date()
-    borrowerid = request.args.get('borrowerid')
-    connection = getCursor()
-    if borrowerid:
-         connection.execute("SELECT * FROM borrowers where borrowerid=%s;",(borrowerid,))
-    else:
-        connection.execute("SELECT * FROM borrowers;")
 
-    borrowerList = connection.fetchall()
-    sql = """SELECT * FROM bookcopies
-        inner join books on books.bookid = bookcopies.bookid
-        WHERE bookcopyid not in (SELECT bookcopyid from loans where returned <> 1 or returned is NULL)
-        OR (bookcopies.format = 'ebook' OR bookcopies.format = 'Audio Book');"""
-    connection.execute(sql)
-    bookList = connection.fetchall()
-    return render_template("addloan.html", loandate = todaydate,borrowers = borrowerList, books= bookList,borrowerid=borrowerid)
-
-@app.route("/loan/add", methods=["POST"])
-def addloan():
-    borrowerid = request.form.get('borrower')
-    bookid = request.form.get('book')
-    loandate = request.form.get('loandate')
-    cur = getCursor()
-    cur.execute("INSERT INTO loans (borrowerid, bookcopyid, loandate, returned) VALUES(%s,%s,%s,0);",(borrowerid, bookid, str(loandate),))
-    return redirect("/currentloans")
-
-@app.route("/listborrowers")
-def listborrowers():
-    connection = getCursor()
-    connection.execute("SELECT * FROM borrowers;")
-    borrowerList = connection.fetchall()
-    return render_template("borrowerlist.html", borrowerlist = borrowerList)
-
-@app.route("/currentloans")
-def currentloans():
-    connection = getCursor()
-    sql=""" select br.borrowerid, br.firstname, br.familyname,  
-                l.borrowerid, l.bookcopyid, l.loandate, l.returned, b.bookid, b.booktitle, b.author, 
-                b.category, b.yearofpublication, bc.format,l.loanid 
-            from books b
-                inner join bookcopies bc on b.bookid = bc.bookid
-                    inner join loans l on bc.bookcopyid = l.bookcopyid
-                        inner join borrowers br on l.borrowerid = br.borrowerid
-            order by br.familyname, br.firstname, l.loandate;"""
-    connection.execute(sql)
-    loanList = connection.fetchall()
-    return render_template("currentloans.html", loanlist = loanList)
-
-# add public access /route
-@app.route("/route")
-def publicroute():
-    return render_template("route.html")
-
-# add staff access /route
-@app.route("/staff")
-def staffroute():
-    return render_template("staffroute.html")
-   
-@app.route("/searchbooks", methods=["POST"])
+@app.route("/searchbooks", methods=["GET"])
 def searchbooks():
     todaydate = datetime.now().date()
-    catalogue=request.form.get('catalogue')
+    catalogue=request.args.get('catalogue')
     selectedcatalogue ="All"
     if catalogue == "title": 
         selectedcatalogue = "b.booktitle" 
     elif catalogue == "author": 
         selectedcatalogue = "b.author"
-    searchterm=request.form.get('search')
+    searchterm=request.args.get('search')
     searchterm="%" +searchterm +"%"
     allsql= """select br.borrowerid, br.firstname, br.familyname,  
                 l.borrowerid, l.bookcopyid, l.loandate, l.returned, b.bookid, b.booktitle, b.author, 
@@ -145,10 +97,71 @@ def searchbooks():
     bookList = connection.fetchall()
     return render_template("booklist.html", booklist = bookList,loandate = todaydate)
     
-@app.route("/searchborrower", methods=["POST"])
+@app.route("/loanbook", methods=["GET"])
+def loanbook():
+    todaydate = datetime.now().date()
+    borrowerid = request.args.get('borrowerid')
+    connection = getCursor()
+    if borrowerid:
+         connection.execute("SELECT * FROM borrowers where borrowerid=%s;",(borrowerid,))
+    else:
+        connection.execute("SELECT * FROM borrowers;")
+    borrowerList = connection.fetchall()
+    sql = """SELECT * FROM bookcopies
+        inner join books on books.bookid = bookcopies.bookid
+        WHERE bookcopyid not in (SELECT bookcopyid from loans where returned <> 1 or returned is NULL)
+        OR (bookcopies.format = 'ebook' OR bookcopies.format = 'Audio Book');"""
+    connection.execute(sql)
+    bookList = connection.fetchall()
+    return render_template("addloan.html", loandate = todaydate,borrowers = borrowerList, books= bookList,borrowerid=borrowerid)
+
+@app.route("/currentloans")
+def currentloans():
+    connection = getCursor()
+    sql=""" select br.borrowerid, br.firstname, br.familyname,  
+                l.borrowerid, l.bookcopyid, l.loandate, l.returned, b.bookid, b.booktitle, b.author, 
+                b.category, b.yearofpublication, bc.format,l.loanid 
+            from books b
+                inner join bookcopies bc on b.bookid = bc.bookid
+                    inner join loans l on bc.bookcopyid = l.bookcopyid
+                        inner join borrowers br on l.borrowerid = br.borrowerid
+            order by br.familyname, br.firstname, l.loandate;"""
+    connection.execute(sql)
+    loanList = connection.fetchall()
+    return render_template("currentloans.html", loanlist = loanList)
+
+@app.route("/loan/add", methods=["POST"])
+def addloan():
+    borrowerid = request.form.get('borrower')
+    bookid = request.form.get('book')
+    loandate = request.form.get('loandate')
+    cur = getCursor()
+    cur.execute("INSERT INTO loans (borrowerid, bookcopyid, loandate, returned) VALUES(%s,%s,%s,0);",(borrowerid, bookid, str(loandate),))
+    return redirect("/currentloans")
+
+@app.route("/returnbook", methods=["POST"])
+def returnbook():
+    loanid = request.form.get('loanid', None)
+    if loanid is None:
+        # return an error message or redirect the user to a different page
+        return redirect("/error")
+    connection = getCursor()
+    sql= "UPDATE loans set returned ='1' where loanid =%s;"
+    connection.execute(sql,(loanid,))
+    return redirect("/currentloans")
+
+#  Borrowers function
+@app.route("/listborrowers")
+def listborrowers():
+    connection = getCursor()
+    connection.execute("SELECT * FROM borrowers;")
+    borrowerList = connection.fetchall()
+    return render_template("borrowerlist.html", borrowerlist = borrowerList)
+
+@app.route("/searchborrower", methods=["GET"])
 def searchborrower():
     connection = getCursor()
-    searchterm = request.form.get('search')
+    searchterm = request.args.get('search')
     if searchterm.isdigit() == True:
         connection.execute("SELECT * FROM borrowers where borrowerid = %s;",(searchterm,))
     else: 
@@ -156,29 +169,6 @@ def searchborrower():
         connection.execute("SELECT * FROM borrowers where firstname LIKE %s or familyname LIKE %s;",(searchterm,searchterm))
     borrowerList = connection.fetchall()
     return render_template("borrowerlist.html", borrowerlist = borrowerList)
-
-@app.route("/addborrower", methods=["GET", "POST"])
-def addborrower():
-    if request.method == "GET":
-        return render_template("addborrower.html")
-    else:
-        first_name = request.form.get("first_name")
-        last_name = request.form.get("last_name")
-        dob = request.form.get("dob")
-        house_number = request.form.get("house_number")
-        street = request.form.get("street")
-        town = request.form.get("town")
-        city = request.form.get("city")
-        postal_code = request.form.get("postal_code")
-
-        # Save the borrower information to a database
-        cur = getCursor()
-        cur.execute("INSERT INTO borrowers (borrowerid, firstname, familyname, dateofbirth,housenumbername,street,town,city,postalcode) VALUES(null,%s,%s,%s,%s,%s,%s,%s,%s);",((str(first_name), str(last_name), dob, str(house_number), str(street), str(town), str(city), str(postal_code),)))
-        updatestatus = "Add borrower sucessful"
-        session['updatestatus'] = updatestatus
-        # Redirect to the borrower detail page
-        return redirect("/listborrowers")
-
 
 @app.route("/borrowerdetail", methods=["GET"])
 def borrowerdetail():
@@ -190,6 +180,23 @@ def borrowerdetail():
     # code to retrieve borrower details and render the template
     return render_template("borrowerdetail.html",borrowerlist=borrowerList,borrowerid=borrowerid, updatestatus=updatestatus)
 
+@app.route("/addborrower", methods=["POST"])
+def addborrower():
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        dob = request.form.get("dob")
+        house_number = request.form.get("house_number")
+        street = request.form.get("street")
+        town = request.form.get("town")
+        city = request.form.get("city")
+        postal_code = request.form.get("postal_code")
+        # Save the borrower information to a database
+        cur = getCursor()
+        cur.execute("INSERT INTO borrowers (borrowerid, firstname, familyname, dateofbirth,housenumbername,street,town,city,postalcode) VALUES(null,%s,%s,%s,%s,%s,%s,%s,%s);",((str(first_name), str(last_name), dob, str(house_number), str(street), str(town), str(city), str(postal_code),)))
+        updatestatus = "Add borrower sucessful"
+        session['updatestatus'] = updatestatus
+        # Redirect to the borrower detail page
+        return redirect("/listborrowers")
 
 @app.route("/updateborrower", methods=["POST"])
 def updateborrower():
@@ -202,35 +209,21 @@ def updateborrower():
     town = request.form.get('town')
     city = request.form.get('city')
     postal_code = request.form.get('postal_code')
-
     # Check if none of the fields is missing or empty
     if not first_name and not last_name and not dob and not house_number and not street and not town and not city and not postal_code:
         updatestatus = "At least one field is required. Please fill in the missing fields and try again."
         session['updatestatus'] = updatestatus
         return redirect("/borrowerdetail?borrowerid="+borrowerid)
-
     # Connect to the database and execute the update query
     connection = getCursor()
     updateborrowersql = "UPDATE borrowers SET firstname = %s, familyname = %s, dateofbirth =%s, housenumbername = %s,street=%s,town= %s,city=%s,postalcode=%s WHERE borrowerid = %s;"
     connection.execute(updateborrowersql, (str(first_name), str(last_name), dob, str(house_number), str(street), str(town), str(city), str(postal_code), borrowerid,))
-
     # Update status and redirect back to the borrower detail page
     updatestatus = "Updated borrower detail successful, Press Enter to continue."
     session['updatestatus'] = updatestatus
     return redirect("/borrowerdetail?borrowerid="+borrowerid)
 
-
-@app.route("/returnbook", methods=["GET"])
-def returnbook():
-    loanid = request.args.get('loanid', None)
-    if loanid is None:
-        # return an error message or redirect the user to a different page
-        return redirect("/error")
-    connection = getCursor()
-    sql= "UPDATE loans set returned ='1' where loanid =%s;"
-    connection.execute(sql,(loanid,))
-    return redirect("/currentloans")
-
+# Reports:  3 reports as below
 @app.route("/overduebookslist")
 def listoverduebooks():
     connection = getCursor()
